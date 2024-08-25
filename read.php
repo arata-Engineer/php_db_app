868a077a5c4dbec5fb759ae9bda16c3e22088ca1
arata-Engineer <arataspace1@gmail.com>
2024-08-25 15:34:08 +0900

    commit_update

--- read.php
+++ read.php
@@ -99,6 +99,7 @@
                      <th>単価</th>
                      <th>在庫数</th>
                      <th>仕入先コード</th>
+                     <th>編集</th>
                  </tr>
                  <?php
                  // 配列の中身を順番に取り出し、表形式で出力する
@@ -109,7 +110,8 @@
                          <td>{$product['product_name']}</td>
                          <td>{$product['price']}</td>
                          <td>{$product['stock_quantity']}</td>
-                         <td>{$product['vendor_code']}</td>                        
+                         <td>{$product['vendor_code']}</td>
+                         <td><a href='update.php?id={$product['id']}'><img src='images/edit.png' alt='編集' class='edit-icon'></a></td> 
                          </tr>                    
                      ";
                      echo $table_row;
